7c47d57c1e6cfaa0893dc3310f059bc13ab65dd9
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-12-03 23:43:47 +0300

    new

--- src/main/java/Test/MyNewServlet.java
+++ src/main/java/Test/MyNewServlet.java
@@ -2,19 +2,27 @@ package Test;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 @WebServlet("/two")
 public class MyNewServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String URI = req.getRequestURI();
+        Enumeration<String> headers = req.getHeaderNames();
+        PrintWriter pw = resp.getWriter();
+        while (headers.hasMoreElements()) {
+            String parameterName = (String) headers.nextElement();
+            String parameterValue = req.getHeader(parameterName);
+            pw.println("<h3>" + parameterName + ": " + parameterValue + "</h3>" + "\n");
+        }
+
+        /*String URI = req.getRequestURI();
         String URL = String.valueOf(req.getRequestURL());
         int session = req.getContentLength();
         String info = req.getPathInfo();
@@ -22,6 +30,8 @@ public class MyNewServlet extends HttpServlet {
         String path = req.getPathInfo();
         String name = req.getServerName();
         Cookie[] cookie = req.getCookies();
+        int port = req.getServerPort();
+
 
 
 
@@ -34,6 +44,7 @@ public class MyNewServlet extends HttpServlet {
         pw.println("path: " + path);
         pw.println("info: " + info);
         pw.println("cookie: " + cookie);
+        pw.println("port: " + port);*/
 
 
 
